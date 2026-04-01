bec0e3418752253e6aadb65433516dc6a837a31d
Gustavo <dev.gustavo97@gmail.com>
2026-04-01 19:13:21 -0300

    valor entre parametros

---
+++ exercicios/day2/ex11.py
@@ -0,0 +1,9 @@
+#Número dentro do intervalo
+
+print("Número dentro do intervalo")
+
+valor = float(input("Digite um número: "))
+if valor >= 10 and valor <= 50:
+    print("Este número está entre 10 e 50")
+else:
+    print("Este número não está entre 10 e 50")
