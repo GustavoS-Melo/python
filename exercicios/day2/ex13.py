c24197fbef65384a366cf574de4bca94d9a1ab7b
Gustavo <dev.gustavo97@gmail.com>
2026-04-01 19:13:58 -0300

    comparação de strings

---
+++ exercicios/day2/ex13.py
@@ -0,0 +1,11 @@
+# Comparar dois nomes
+
+print("Comparação de nomes")
+
+nome = input("Digite um nome: ")
+nome1 = input("Digite outro nome: ")
+
+if nome == nome1: 
+    print("Este dois nomes são iguais")
+else:
+    print("Estes dois nomes são diferentes")
