39656775bf8158507805424ce6977f4b7c630220
Gustavo <dev.gustavo97@gmail.com>
2026-04-01 19:10:00 -0300

    maior numero

---
+++ exercicios/day2/ex2.py
@@ -0,0 +1,11 @@
+# Maior número
+
+print("Maior número:")
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num1 > num2:
+    print("O primeiro número é maior que o segundo")
+else:
+    print("O segundo número é maior que o primeiro")
