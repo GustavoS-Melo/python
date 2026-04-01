160352ff52d2d98234ee11c2454bd6563ac438ab
Gustavo <dev.gustavo97@gmail.com>
2026-04-01 19:10:30 -0300

    verificação de aprovação

---
+++ exercicios/day2/ex3.py
@@ -0,0 +1,9 @@
+#Aprovado ou reprovado
+
+print("Aprovado ou reprovado: ")
+
+nota = int(input("Digite a sua nota: "))
+if nota >= 7:
+    print("Você esta aprovado!")
+else:
+    print("Você não está aprovado!")
